aca79588c15e5d37875d749cf8524a68d260729d
Marton Tasnadi <marton.tasnadi@gmail.com>
2018-07-06 09:28:33 +0200

    PaymentForm fix and add PHPDoc

--- src/PaymentForm.php
+++ src/PaymentForm.php
@@ -1,8 +1,18 @@
 <?php
 
+/**
+ * Form validation manager
+ */
 class PaymentForm
 {
+    /**
+     * @var string Original HUF amount
+    */
     private $amount;
+
+    /**
+     * Form data (currently $_POST)
+    */
     private $formData;
 
     public function __construct($formData)
@@ -22,36 +32,59 @@ class PaymentForm
         return isset($this->formData[$input]) ? $this->formData[$input] : null;
     }
 
+    /**
+     * Card Number validator manager.
+     *
+     * @return string|boolean
+     */
     public function cardNumberValidation()
     {
-        $cardNumber = $this->inputExist($this->formData['card_number']);
+        $cardNumber = $this->inputExist('card_number');
         $cardNumberValidator = new CardNumberValidator($cardNumber);
+
         return $cardNumberErrorMessage = $cardNumberValidator->validate();
     }
 
+    /**
+     * Card Expiration Validator manager.
+     *
+     * @return string|boolean
+     */
     public function cardExpirationValidator()
     {
-        $expirationYear = $this->inputExist($this->formData['card_expiration_year']);
-        $expirationMonth = $this->inputExist($this->formData['card_expiration_month']);
+        $expirationYear = $this->inputExist('card_expiration_year');
+        $expirationMonth = $this->inputExist('card_expiration_month');
         $cardExpirationValidator = new CardExpirationValidator($expirationYear, $expirationMonth);
+
         return $cardExpirationErrorMessage = $cardExpirationValidator->validate();
     }
 
+    /**
+     * Amount Validator manager.
+     *
+     * @return string|boolean
+     */
     public function amountValidator()
     {
-        $amount = $this->inputExist($this->formData['amount']);
+        $amount = $this->inputExist('amount');
         $amountValidator = new AmountValidator($amount);
         $this->amount = $amount;
+
         return $amountErrorMessage = $amountValidator->validate();
     }
 
+    /**
+     * Exchange manager.
+     *
+     * @return string|boolean
+     */
     public function exchangeAmount()
     {
         $amountInEur = null;
         if (!is_null($this->amount)) {
             $MNBCurrencyExchange = new MNBCurrencyExchange($this->amount);
-            return $amountInEur = $MNBCurrencyExchange->calculateNewAmount();
 
+            return $amountInEur = $MNBCurrencyExchange->calculateNewAmount();
         }
     }
 }
